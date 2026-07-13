e9f59321933cbc8e11a002b842adc7d4ffae8ff1
Nicklas Hansen <hello@nicklashansen.com>
2026-07-13 15:26:47 -0700

    fix q ensemble weight init

--- tdmpc2/common/init.py
+++ tdmpc2/common/init.py
@@ -9,11 +9,6 @@ def weight_init(m):
 			nn.init.constant_(m.bias, 0)
 	elif isinstance(m, nn.Embedding):
 		nn.init.uniform_(m.weight, -0.02, 0.02)
-	elif isinstance(m, nn.ParameterList):
-		for i,p in enumerate(m):
-			if p.dim() == 3: # Linear
-				nn.init.trunc_normal_(p, std=0.02) # Weight
-				nn.init.constant_(m[i+1], 0) # Bias
 
 
 def zero_(params):
--- tdmpc2/common/world_model.py
+++ tdmpc2/common/world_model.py
@@ -27,7 +27,7 @@ class WorldModel(nn.Module):
 		self._reward = layers.mlp(cfg.latent_dim + cfg.action_dim + cfg.task_dim, 2*[cfg.mlp_dim], max(cfg.num_bins, 1))
 		self._termination = layers.mlp(cfg.latent_dim + cfg.task_dim, 2*[cfg.mlp_dim], 1) if cfg.episodic else None
 		self._pi = layers.mlp(cfg.latent_dim + cfg.task_dim, 2*[cfg.mlp_dim], 2*cfg.action_dim)
-		self._Qs = layers.Ensemble([layers.mlp(cfg.latent_dim + cfg.action_dim + cfg.task_dim, 2*[cfg.mlp_dim], max(cfg.num_bins, 1), dropout=cfg.dropout) for _ in range(cfg.num_q)])
+		self._Qs = layers.Ensemble([layers.mlp(cfg.latent_dim + cfg.action_dim + cfg.task_dim, 2*[cfg.mlp_dim], max(cfg.num_bins, 1), dropout=cfg.dropout).apply(init.weight_init) for _ in range(cfg.num_q)])
 		self.apply(init.weight_init)
 		init.zero_([self._reward[-1].weight, self._Qs.params["2", "weight"]])
 
